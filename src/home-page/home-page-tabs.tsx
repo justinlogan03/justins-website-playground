6b31cb0a6ba5964c8d2c088ef70787ff7f861b99
jlogan-costar <111456962+jlogan-costar@users.noreply.github.com>
2024-06-01 22:02:50 -0400

    styling

--- src/home-page/home-page-tabs.tsx
+++ src/home-page/home-page-tabs.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const HomePageTabs = () => {
-  return <div className="flex w-full bg-green-300">Tabs Placeholder</div>;
+  return (
+    <div className="flex w-full bg-gray-500" style={{ minHeight: "48rem" }}>
+      Tabs Placeholder
+    </div>
+  );
 };
