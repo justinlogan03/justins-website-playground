bbd96308005a6ff1db42ea0e42a47794858907d1
jlogan-costar <111456962+jlogan-costar@users.noreply.github.com>
2024-06-01 22:11:32 -0400

    tabs shell

--- src/home-page/home-page-tabs.tsx
+++ src/home-page/home-page-tabs.tsx
@@ -2,8 +2,24 @@ import React from "react";
 
 export const HomePageTabs = () => {
   return (
-    <div className="flex w-full bg-gray-500" style={{ minHeight: "48rem" }}>
-      Tabs Placeholder
+    <div>
+      <div className="flex w-full h-12 bg-blue-300">
+        <div
+          className="w-32 bg-gray-500 border-black border-x"
+          style={{ borderTopLeftRadius: "25%", borderTopRightRadius: "25%" }}
+        ></div>
+        <div
+          className="w-32 bg-gray-500 border-b border-black border-x"
+          style={{ borderTopLeftRadius: "25%", borderTopRightRadius: "25%" }}
+        ></div>
+        <div
+          className="w-32 bg-gray-500 border-b border-black border-x"
+          style={{ borderTopLeftRadius: "25%", borderTopRightRadius: "25%" }}
+        ></div>
+      </div>
+      <div className="flex w-full bg-gray-500 " style={{ minHeight: "48rem" }}>
+        Tabs Placeholder
+      </div>
     </div>
   );
 };
