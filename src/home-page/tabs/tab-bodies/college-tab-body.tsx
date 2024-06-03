2201a5da3b1936170ff07c8c11779c8de8793cf5
jlogan-costar <111456962+jlogan-costar@users.noreply.github.com>
2024-06-03 07:50:04 -0400

    tab body styling

--- src/home-page/tabs/tab-bodies/college-tab-body.tsx
+++ src/home-page/tabs/tab-bodies/college-tab-body.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { BodyTemplate } from "./body-template";
+import ArticleIcon from "@mui/icons-material/Article";
 
 export const CollegeTabBody = () => {
   return (
-    <BodyTemplate>
-      <div>College Tab Body</div>
+    <BodyTemplate headerLabel="College Sports">
+      <h2 className="flex w-1/3 p-2 text-lg font-bold bg-blue-200 border-b-2 border-black">
+        <p className="mr-2">
+          <ArticleIcon />
+        </p>
+        <p> My Opinions</p>
+      </h2>
     </BodyTemplate>
   );
 };
