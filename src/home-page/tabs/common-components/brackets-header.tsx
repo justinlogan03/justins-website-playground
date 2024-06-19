d737a9bc83ffcc2babb9e4a26263bda8c4e1b39b
jlogan-costar <111456962+jlogan-costar@users.noreply.github.com>
2024-06-19 16:38:37 -0400

    placeholder items

--- src/home-page/tabs/common-components/brackets-header.tsx
+++ src/home-page/tabs/common-components/brackets-header.tsx
@@ -4,11 +4,27 @@ import "/src/style.css";
 
 export const BracketsHeader = () => {
   return (
-    <h2 className="flex w-1/3 p-2 mb-48 text-lg font-bold teal-text">
-      <p className="mr-2 icon-color">
-        <ScoreboardIcon />
-      </p>
-      <p>Brackets, Pools, and Fun</p>
-    </h2>
+    <div>
+      <h2 className="flex w-1/3 p-2 mb-4 text-lg font-bold teal-text">
+        <p className="mr-2 icon-color">
+          <ScoreboardIcon />
+        </p>
+        <p>Brackets, Pools, and Fun</p>
+      </h2>
+      <div className="flex mb-4">
+        <image className="w-48 mr-4 bg-gray-400 h-36 rounded-xl min-w-48"></image>
+        <div className="p-4 selectable-background rounded-xl">
+          <h3 className="font-bold teal-text">NBA Playoff Pool</h3>
+          <p className="text-white ">
+            Here is the text for the NBA playoff pool. I will put real text here
+            later but this is where a description will live. There will be a
+            much larger description here with lots of text. I'm hoping this goes
+            to multiple lines so I can see what it looks like. If it doesn't, I
+            will have to input more text. Click to access the NBA Playoff Pool
+            360.
+          </p>
+        </div>
+      </div>
+    </div>
   );
 };
--- src/home-page/tabs/tab-bodies/body-template.tsx
+++ src/home-page/tabs/tab-bodies/body-template.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const BodyTemplate = ({ children, headerLabel }: Props) => {
   return (
-    <div className="w-full mx-8 my-6 ">
+    <div className="mx-8 my-6 ">
       <h1 className="w-1/2 p-2 mb-4 text-3xl font-bold border-b-2 content-border teal-text">
         {headerLabel}
       </h1>
