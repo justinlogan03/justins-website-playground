cdfe7f54d9e4d9b380ac8012879b4f6683b23d92
Justin <70607137+justinlogan03@users.noreply.github.com>
2024-12-07 16:45:30 -0500

    not sure

--- src/just-for-fun/would-you-rather/would-you-rather-contents.tsx
+++ src/just-for-fun/would-you-rather/would-you-rather-contents.tsx
@@ -1,37 +1,45 @@
 import React, { useState } from "react";
-import AutorenewIcon from "@mui/icons-material/Autorenew";
 
 import { getRandomPrompts } from "./helpers/get-random-prompts";
 import { CurrentPrompt } from "./types";
+import { PromptCard } from "./components/prompt-card";
+import { RefreshButton } from "./components/refresh-button";
 
 export const WouldYouRatherContents = () => {
+  const [isPromptOneLocked, setIsPromptOneLocked] = useState<boolean>(false);
+  const [isPromptTwoLocked, setIsPromptTwoLocked] = useState<boolean>(false);
   const [currentPrompts, setCurrentPrompts] = useState<CurrentPrompt>(
-    getRandomPrompts()
+    getRandomPrompts(isPromptOneLocked, isPromptTwoLocked)
   );
 
   const onRefresh = () => {
-    setCurrentPrompts(getRandomPrompts(currentPrompts));
+    setCurrentPrompts(
+      getRandomPrompts(isPromptOneLocked, isPromptTwoLocked, currentPrompts)
+    );
   };
   return (
     <div className="w-full flex">
       <div className="w-1/2 tab-height text-white justify-center items-center flex">
-        {currentPrompts.optionOne?.prompt}
+        <PromptCard
+          label={currentPrompts.optionOne?.prompt}
+          isLocked={isPromptOneLocked}
+          setIsLocked={setIsPromptOneLocked}
+        />
       </div>
       <div className="tab-height justify-center items-center flex">
         <div>
           <h1 className="text-white text-3xl font-bold text-center mb-8">
             Or...
           </h1>
-          <button
-            className="selectable-background h-12 w-12 rounded-xl icon-color"
-            onClick={onRefresh}
-          >
-            <AutorenewIcon fontSize="large" />
-          </button>
+          <RefreshButton onRefresh={onRefresh} />
         </div>
       </div>
       <div className="w-1/2 tab-height text-white justify-center items-center flex">
-        {currentPrompts.optionTwo?.prompt}
+        <PromptCard
+          label={currentPrompts.optionTwo?.prompt}
+          isLocked={isPromptTwoLocked}
+          setIsLocked={setIsPromptTwoLocked}
+        />
       </div>
     </div>
   );
