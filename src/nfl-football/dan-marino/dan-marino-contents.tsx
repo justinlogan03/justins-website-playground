8e1d8daaf9f246e15ff756faf03f4c07cca0961d
Justin <70607137+justinlogan03@users.noreply.github.com>
2025-01-18 14:29:31 -0500

    dan marino article shell

---
+++ src/nfl-football/dan-marino/dan-marino-contents.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const DanMarinoContents = () => {
+  return (
+    <div className="w-full flex">
+      <div className="text-white">TEST</div>
+    </div>
+  );
+};
